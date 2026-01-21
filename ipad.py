144ceeaffb1d687e147faca762bd52a44d4418e7
User <user@OaksMacBook-AirM1.local>
2026-01-21 20:35:30 +0630

    first commit

---
+++ ipad.py
@@ -0,0 +1,79 @@
+print("genfoxiosshop")
+print("Min ga lar pr shint")
+ipadsecondmodel= ["ipadm1","ipadm2","ipadm3","ipadm4","ipadm5"]
+# print(ipadsecondmodel[0])
+# print(ipadsecondmodel[1])
+# print(ipadsecondmodel[2])
+# print(ipadsecondmodel[3])
+for a in ipadsecondmodel:
+    print(a)
+userchoicemodel=(input("choice ipad model")) 
+if userchoicemodel == "ipadm1":
+    choiceipadcolor=(input("silver,black"))
+    if choiceipadcolor== "silver":
+        userchoicecondition=(input("80%,100%"))
+        if userchoicecondition=="80%":
+            print("ipadm1 80% zay -1800k")
+        elif userchoicecondition=="100%":
+            print("ipadm1 100% zay-2400k")
+    elif choiceipadcolor=="black":
+        userchoicecondition=(input("80%,100%"))
+        if userchoicecondition=="80%":
+            print("ipadm1 80% zay -1800k")
+        elif userchoicecondition=="100%":
+            print("ipadm1 100% zay-2400k")  
+elif userchoicemodel=="ipadm2":
+    choiceipadcolor=input("dark  blue")
+    if choiceipadcolor=="dark blue":
+        userchoicecondition=(input("85%,80%"))
+    if  userchoicecondition=="85%":
+            print("ipadm2 85% zay- 26thein")
+    elif userchoicecondition=="80%":
+         print("ipadm2 80% zay-  30 thein")
+if userchoicemodel=="ipadm3":
+    choiceipadcolor=input("black")
+    if choiceipadcolor=="black":
+        userchoicecondition=(input("80%,90%"))
+    if userchoicecondition=="80%":
+        print("ipadm3 80% zay=32thein")
+    elif userchoicecondition=="90%":
+          print("ipadm3 90% zay=40thein")
+          print("when you buy ipadm3 giff applepencil")
+elif  userchoicemodel=="ipadm4":
+    userchoicecolor=input("blue")
+    if userchoicecolor=="blue":
+        userchoicecondition=(input("brandnew"))    
+        print("brand new ipad m4 zay=52thein")
+if userchoicemodel=="ipadm5":
+    userchoicecolor=input("blue,black,grey")
+    if userchoicecolor=="blue":
+        print("ipadm5 pre order price 65thein")
+    elif userchoicecolor=="black":
+         print("ipadm5 pre order price 65thein")
+    elif userchoicecolor=="grey":
+           print("ipadm5 pre order price 65thein")
+
+      
+      
+
+
+
+
+         
+
+         
+       
+
+   
+
+
+
+
+
+
+    
+
+
+
+
+
