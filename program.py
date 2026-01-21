45aecf36db4d100730301d69c5aefade7f9f7192
User <user@OaksMacBook-AirM1.local>
2026-01-21 20:56:11 +0630

    file post lyk b

---
+++ program.py
@@ -0,0 +1,17 @@
+print("mingalar pr")
+numberthree=4.55
+numberone=7.55
+animal="blue color"
+oaksoeoo=1
+print(oaksoeoo)
+earphone=45
+print(earphone)
+print(45)
+earphonered=4
+earphoneblue=7
+print("earphonered+earphoneblue")
+print(earphonered+earphoneblue)
+print(earphonered-earphoneblue)
+print(earphonered*earphonered)
+print(earphoneblue*earphoneblue / earphonered)
+
